880ecc262ecdc86afcac7c6d24318401b3773da3
codyMatheney <mathenec4972@student.faytechcc.edu>
2024-09-19 10:40:41 -0400

    Add files via upload

---
+++ P1LAB1 cody matheney.py
@@ -0,0 +1,13 @@
+#mallory
+#9/17/2024
+#get input from user and display output
+
+#get first name from user
+first_name=input("enter your first name:")
+
+#get last name from user
+last_name=input("enter your last name:")
+
+#display info to user
+    
+print("hellow,",first_name,last_name+"! welcome to CTI-110")
